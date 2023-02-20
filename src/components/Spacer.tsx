c0c8ea87ec5e24e1726968b988994846d878b37a
seongjun.oh <tiger8708@gmail.com>
2023-02-20 20:17:17 +0900

    로또 번호 추출 UI 추가

--- src/components/Spacer.tsx
+++ src/components/Spacer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export const Spacer = ({ horizontal, space }: { horizontal: boolean; space: number }) => {
+export const Spacer = ({ horizontal, space }: { horizontal?: boolean; space: number }) => {
     if (horizontal) {
         return <View style={{ marginLeft: space }} />;
     }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
+import { Button } from '../components/Button';
 import { Header } from '../components/Header/Header';
+import { Spacer } from '../components/Spacer';
+import { Typography } from '../components/Typography';
 
 export const HomeScreen = () => {
+    const onPressGetNumber = useCallback(() => {}, []);
     return (
         <View style={{ flex: 1 }}>
             <Header>
                 <Header.Title title="HOME" />
             </Header>
+            <View
+                style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    paddingHorizontal: 12,
+                }}>
+                <View
+                    style={{
+                        height: 250,
+                        flexDirection: 'column',
+                        paddingHorizontal: 24,
+                        backgroundColor: 'white',
+                        borderColor: 'gray',
+                    }}>
+                    <View
+                        style={{
+                            flex: 1,
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                        }}>
+                        {[1, 2, 3, 4, 5].map(item => {
+                            return (
+                                <View
+                                    key={`lotto-${item}`}
+                                    style={{
+                                        backgroundColor: 'black',
+                                        width: 40,
+                                        height: 40,
+                                        borderRadius: 20,
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                    }}>
+                                    <Typography fontSize={20} color="white">
+                                        {item}
+                                    </Typography>
+                                </View>
+                            );
+                        })}
+                    </View>
+                </View>
+                <Spacer space={20} />
+                <Button onPress={onPressGetNumber}>
+                    <View style={{ backgroundColor: 'black', paddingVertical: 24, alignItems: 'center' }}>
+                        <Typography color="white" fontSize={18}>
+                            로또 번호 추출하기
+                        </Typography>
+                    </View>
+                </Button>
+            </View>
         </View>
     );
 };
